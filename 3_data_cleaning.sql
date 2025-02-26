a09184a54cd4a09994eaf034844d9d8eb260e26a
Wifaq Md Nor <57818364+wifaqmn@users.noreply.github.com>
2025-02-26 10:24:42 +0800

    Update 3_data_cleaning.sql

--- 3_data_cleaning.sql
+++ 3_data_cleaning.sql
@@ -13,31 +13,7 @@ DROP TABLE IF EXISTS cyclistic-project-50.trip_data.customer_data_2024_cleaned;
 CREATE TABLE IF NOT EXISTS cyclistic-project-50.trip_data.customer_data_2024_cleaned AS (
   SELECT 
     a.ride_id, rideable_type, started_at, ended_at, 
-    ride_length,
-    CASE EXTRACT(DAYOFWEEK FROM started_at) 
-      WHEN 1 THEN 'SUN'
-      WHEN 2 THEN 'MON'
-      WHEN 3 THEN 'TUES'
-      WHEN 4 THEN 'WED'
-      WHEN 5 THEN 'THURS'
-      WHEN 6 THEN 'FRI'
-      WHEN 7 THEN 'SAT'    
-    END AS day_of_week,
-    CASE EXTRACT(MONTH FROM started_at)
-      WHEN 1 THEN 'JAN'
-      WHEN 2 THEN 'FEB'
-      WHEN 3 THEN 'MAR'
-      WHEN 4 THEN 'APR'
-      WHEN 5 THEN 'MAY'
-      WHEN 6 THEN 'JUN'
-      WHEN 7 THEN 'JUL'
-      WHEN 8 THEN 'AUG'
-      WHEN 9 THEN 'SEP'
-      WHEN 10 THEN 'OCT'
-      WHEN 11 THEN 'NOV'
-      WHEN 12 THEN 'DEC'
-    END AS month,
-    start_station_name, end_station_name, 
+    ride_length, start_station_name, end_station_name, 
     start_lat, start_lng, end_lat, end_lng, member_casual,
   FROM cyclistic-project-50.trip_data.customer_data_2024 a
   JOIN (
